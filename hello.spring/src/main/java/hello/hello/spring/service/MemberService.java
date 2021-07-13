c2f21442cfa8d47907020470f25bd6b5eb193382
coding han joom <70880034+heojumi@users.noreply.github.com>
2021-07-14 03:08:49 +0900

    study 3

---
+++ hello.spring/src/main/java/hello/hello/spring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hello.spring.service;
+
+import hello.hello.spring.domain.Member;
+import hello.hello.spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    //DI
+
+
+    //회원가입
+    public Long join(Member member){
+        //중복 회원은 안된다
+        validateDuplicateMember(member);//중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+}
